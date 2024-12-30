48de6b38c93d77c0d2eedb0c05696d46f351f681
loleik <lolei+github@lolei.uk>
2024-12-30 14:46:35 +0000

    Disassembler

--- src/disassembler.rs
+++ src/disassembler.rs
@@ -1,23 +1,99 @@
 use crate::trie::DisTrie;
 
 // Main disassembler function. Takes the binary vector as input.
+// Jumps and such won't work as I'm just plainly going through the binary
+// instruction by instruction, only ensuring we jump past any addresses or data.
 pub fn disassembler(data: &Vec<u8>) {
     let prefix_trie: DisTrie = make_tree(); // Creates the trie.
 
     let mut i: usize = 0;
 
-    // Just printing the first 20 instructions for now.
-    // No need to spam out the whole file for now while debugging.
-    while i < 20 {
-        println!("{}", data[i]);
-
-        println!("{:?} : {:02X}",
-            prefix_trie.get_instruction(data[i]),
-            data[i]);
-
+    // Loop through all the provided data.
+    while i < data.len() {
         if let Some(current) = prefix_trie.get_instruction(data[i]) {
             let arr: Vec<&str> = current.split(",").collect();
-            println!("{} {} {} \n", arr[2], arr[3], arr[4]);
+
+            // Memory addresses are stored as little endian values.
+            // I am honestly not sure if I'm handling addresses in the right endian or not..
+            match arr[1] {
+                "ABS" => { // Absolute
+                    println!(
+                        "{} ${:04X}",
+                        arr[0],
+                        ((data[i + 1] as u16) << 8) | (data[i + 2] as u16)
+                    )
+                }
+                "ABSX" | "ABSY" => { // Absolute X and Y
+                    println!(
+                        "{} ${:04X},{}",
+                        arr[0],
+                        ((data[i + 1] as u16) << 8) | (data[i + 2] as u16),
+                        arr[1].chars().last().unwrap()
+                    )
+                }
+                "IND" => { // Indirect
+                    println!(
+                        "{} (${:04X})",
+                        arr[0],
+                        ((data[i + 1] as u16) << 8) | (data[i + 2] as u16),
+                    )
+                }
+                "IMP" => { // Implicit
+                    println!(
+                        "{}",
+                        arr[0],
+                    )
+                }
+                "ACC" => { // Accumulator
+                    println!(
+                        "{} A",
+                        arr[0],
+                    )
+                }
+                "IMM" => { // Immediate
+                    println!(
+                        "{} #{:02X}",
+                        arr[0],
+                        data[i + 1],
+                    )
+                }
+                "ZP" => { // Zero Page
+                    println!(
+                        "{} ${:02X}",
+                        arr[0],
+                        data[i + 1],
+                    )
+                }
+                "ZPX" | "ZPY" => { // Zero Page X and Y
+                    println!(
+                        "{} ${:02X},{}",
+                        arr[0],
+                        data[i + 1],
+                        arr[1].chars().last().unwrap(),
+                    )
+                }
+                "INDX" => { // Indexed Indirect
+                    println!(
+                        "{} (${:02X},X)",
+                        arr[0],
+                        data[i + 1],
+                    )
+                }
+                "INDY" => { // Indirect Indexed
+                    println!(
+                        "{} (${:02X}),Y",
+                        arr[0],
+                        data[i + 1],
+                    )
+                }
+                // I haven't written a branch for REL yet.
+                _ => println!(
+                        "{:?} : {:?} : {:02X}",
+                        arr[0], arr[1],
+                        data[i]
+                )
+            }
+
             if arr[2].parse::<usize>().unwrap() > 1 {
                 i += arr[2].parse::<usize>().unwrap();
             } else {
@@ -185,6 +261,6 @@ fn make_tree() -> DisTrie {
     dis_trie.insert(&0x84_u8, "STY,ZP,2,3,czidbvn".to_string());
     dis_trie.insert(&0x94_u8, "STY,ZPX,2,4,czidbvn".to_string());
     dis_trie.insert(&0x8c_u8, "STY,ABS,3,4,czidbvn".to_string());
-        
+                                
     dis_trie
 }
--- src/helpers/trie_help.py
+++ src/helpers/trie_help.py
@@ -13,11 +13,6 @@ with open('6502ops.csv', newline='') as csvfile:
             row['flags'],
         ]
 
-sorted_dict = {k: ops[k] for k in sorted(ops)}
-
-for key, value in sorted_dict.items():
-    print(f'{key}: {value}')
-
 with open('output.txt', 'w') as file:
     for op in ops:
         line = f"dis_trie.insert(&{op}_u8, \"{",".join(ops[op])}\".to_string());"
