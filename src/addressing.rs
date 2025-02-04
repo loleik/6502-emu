e9677101a678d57132f2d169dbd70e31217342a5
loleik <lolei+github@lolei.uk>
2025-02-04 12:04:42 +0000

    Indirect addressing

--- src/addressing.rs
+++ src/addressing.rs
@@ -11,11 +11,11 @@ pub fn zero_page(core: &mut Core) -> u8 {
 }
 
 pub fn zero_page_x(core: &mut Core) -> u8 {
-    core.memory[(core.pc as usize + 1) + core.ix as usize]
+    core.memory[(core.pc as usize + 1).wrapping_add(core.ix as usize)]
 }
 
 pub fn zero_page_y(core: &mut Core) -> u8 {
-    core.memory[(core.pc as usize + 1) + core.iy as usize]
+    core.memory[(core.pc as usize + 1).wrapping_add(core.iy as usize)]
 }
 
 pub fn absolute(core: &mut Core) -> u16 {
@@ -53,12 +53,33 @@ pub fn indirect(core: &mut Core) -> u16 {
     ((th as u16) << 8) | (tl as u16)
 }
 
-/*
-pub fn x_indirect(core: &mut Core) -> u16 {
+pub fn x_indirect(core: &mut Core) -> u8 {
+    let zpx: u8 = core.memory[core.pc as usize + 1].wrapping_add(core.ix);
+
+    // Get the low and high bytes of the pointer and combine them
+    let ptl: u8 = core.memory[zpx as usize];
+    let pth: u8 = core.memory[(zpx + 1) as usize];
+
+    let pointer: u16 = ((pth as u16) << 8) | (ptl as u16);
+
+    // Get the low and high bytes of the actual target and combine them
+    let tl: u8 = core.memory[pointer as usize];
+    let th: u8 = core.memory[(pointer + 1) as usize];
+
+    core.memory[(((th as u16) << 8) | (tl as u16)) as usize]
 }
-pub fn indirect_y(core: &mut Core) -> u16 {
+
+pub fn indirect_y(core: &mut Core) -> u8 {
+    let zp: u8 = core.memory[core.pc as usize + 1];
+
+    // Get the low and high bytes of the pointer and combine them
+    let ptl: u8 = core.memory[zp as usize];
+    let pth: u8 = core.memory[(zp + 1) as usize];
+
+    let pointer: u16 = ((pth as u16) << 8) | (ptl as u16);
+    
+    core.memory[pointer.wrapping_add(core.iy as u16) as usize]
 }
-*/
 
 pub fn relative(core: &mut Core) -> i8 {
     core.memory[(core.pc as usize) + 1] as i8
